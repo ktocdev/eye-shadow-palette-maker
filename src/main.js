9de1e8457aa4dd877830978cfff9b8b6876af17a
katie oconnor <katina.surise@gmail.com>
2025-08-22 12:41:10 -0500

    ESPM-1 - remove package, improve name

--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: App
-    }
-  ]
-})
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
